0431e0ce907674669e73a829c1dd9eb337cd57c1
Mark Arnold <mark.arnold@accesshq.com>
2022-12-09 11:26:10 +1000

    Add DriverFactory to UnitTest

---
+++ WebTestsQldDec22/DriverFactory.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace WebTestsQldDec22
+{
+    internal static class DriverFactory
+    {
+        internal static IWebDriver Build(TestContext testContext)
+        {
+            string browserType = (string)testContext.Properties["browserType"];
+            switch (browserType)
+            {
+                case "local":
+                    return new ChromeDriver();
+                case "remote":
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.AddArguments("--no-sandbox");
+                    chromeOptions.BrowserVersion = "108.0";
+                    chromeOptions.PlatformName = "ANY";
+                    return new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), chromeOptions);
+                default:
+                    throw new Exception($"Browser type of {browserType} must be 'local' or 'remote'");
+            }
+        }
+    }
+}
